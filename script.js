379ffdb43045cb506c061f23632ba14fac759190
mrndolo <callistomutunga@gmail.com>
2024-08-04 12:38:00 +0300

    Added data to be used to update the DOM

---
+++ script.js
@@ -0,0 +1,8 @@
+const containerEl = document.querySelector(".container");
+
+const careers = ["Web Developer", "Data Scientist", "Tech Specialist"];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
